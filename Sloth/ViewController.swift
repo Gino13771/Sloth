17d79a1cca17d888ce4717ddbfae63988110e5ad
Gino <gino13771@gmail.com>
2023-11-14 23:19:25 +0800

    Initial Commit

---
+++ Sloth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sloth
+//
+//  Created by 凱聿蔡凱聿 on 2023/10/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
